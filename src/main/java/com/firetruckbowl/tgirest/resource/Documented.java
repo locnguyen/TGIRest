e788abac9a1509ba119ccb597de174f9701b77dc
Loc Nguyen <lochnguyen@gmail.com>
2011-09-12 19:34:30 -0700

    Added HttpHeaders parameter to the method signature so implementers can access HTTP headers.
    Having access to the headers could prove to be useful. For example,
    developers can check what the Accept-Language or Accept-Encoding is and
    handle accordingly.

--- src/main/java/com/firetruckbowl/tgirest/resource/Documented.java
+++ src/main/java/com/firetruckbowl/tgirest/resource/Documented.java
@@ -2,6 +2,7 @@ package com.firetruckbowl.tgirest.resource;
 
 import javax.ws.rs.OPTIONS;
 import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
 
@@ -17,8 +18,10 @@ public interface Documented {
   /**
    *
    * @param uriInfo injected by the Servlet, provides information useful for building URIs
+   * @param httpHeaders injected by the Servlet, gives access to HTTP headers from the request
+   *    and might be useful for checking things like Accepted Language, etc.
    * @return the class' documentation
    */
   @OPTIONS
-  Response getDocumentation(@Context UriInfo uriInfo);
+  Response getDocumentation(@Context UriInfo uriInfo, @Context HttpHeaders httpHeaders);
 }
